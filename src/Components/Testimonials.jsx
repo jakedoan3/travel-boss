7b7388b6b22d701b2081601a6b1b71f5623f88a7
Jake Doan <jakedoan3@gmail.com>
2024-02-22 15:10:00 -0600

    added testimonial5

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -16,7 +16,7 @@ const Testimonials = () => {
           never forget!"
         </p>
         <h4 className="testimonial-author">~ Kate, Michigan</h4>
-        <br/>
+        <br />
       </div>
       <div className="testimonial">
         <img
@@ -25,13 +25,22 @@ const Testimonials = () => {
           alt="testimonial"
         />
         <p className="testimonial-text">
-        "Nicki has meticulously planned our annual girls' trip for the past 25 years, 25 different locations, spanning coast to coast. We’ve seen city lights, remote hideaways, and everything in between. That's a wealth of experience that few can rival.
-        <br/>
-        <br/>
-        But it's not just about the destinations; it's the delicate art of managing the diverse interests and personalities of 5-10 women. Nicki doesn't just strike a balance; she conjures a harmonious symphony of experiences, seamlessly weaving activities and relaxation into each trip.
-        <br/>
-        <br/>
-        Choosing Nicki as your guide is an investment in a journey that's not just memorable, but flawless. With her at the helm, you can relinquish your travel worries and savor every moment of the adventure."
+          "Nicki has meticulously planned our annual girls' trip for the past 25
+          years, 25 different locations, spanning coast to coast. We’ve seen
+          city lights, remote hideaways, and everything in between. That's a
+          wealth of experience that few can rival.
+          <br />
+          <br />
+          But it's not just about the destinations; it's the delicate art of
+          managing the diverse interests and personalities of 5-10 women. Nicki
+          doesn't just strike a balance; she conjures a harmonious symphony of
+          experiences, seamlessly weaving activities and relaxation into each
+          trip.
+          <br />
+          <br />
+          Choosing Nicki as your guide is an investment in a journey that's not
+          just memorable, but flawless. With her at the helm, you can relinquish
+          your travel worries and savor every moment of the adventure."
         </p>
         <h4 className="testimonial-author">~ Jen M., Arizona</h4>
         <br />
@@ -43,10 +52,23 @@ const Testimonials = () => {
           alt="testimonial"
         />
         <p className="testimonial-text">
-          "We contacted Nicki at TravelBoss to arrange a family reunion of 40+ adults and children from around the United States. Two challenges presented themselves: 1) Where to host the event and 2) the entire family wanted to be in the same home. Nicki was more than capable of meeting these challenges and more. At various points in the planning stage, I admit I was skeptical, but Travel Boss said, “Trust the process, I got this.” She did and we were so pleased with the location as well as the home that accommodated us all. The Travel Boss fielded multiple emails with patience, professionalism, and provided clarity and reassurances.
-          <br/>
-          <br/>
-          Nicki anticipated our needs before we thought of them: handicap parking, closing off sections of the home to provide safety for babies and toddlers, easy access to balconies, and finding an easy-to-navigate home for the blind). I recommend Travel Boss for any of your traveling plans."
+          "We contacted Nicki at TravelBoss to arrange a family reunion of 40+
+          adults and children from around the United States. Two challenges
+          presented themselves: 1) Where to host the event and 2) the entire
+          family wanted to be in the same home. Nicki was more than capable of
+          meeting these challenges and more. At various points in the planning
+          stage, I admit I was skeptical, but Travel Boss said, “Trust the
+          process, I got this.” She did and we were so pleased with the location
+          as well as the home that accommodated us all. The Travel Boss fielded
+          multiple emails with patience, professionalism, and provided clarity
+          and reassurances.
+          <br />
+          <br />
+          Nicki anticipated our needs before we thought of them: handicap
+          parking, closing off sections of the home to provide safety for babies
+          and toddlers, easy access to balconies, and finding an
+          easy-to-navigate home for the blind). I recommend Travel Boss for any
+          of your traveling plans."
         </p>
         <h4 className="testimonial-author">~ Ramona H., Virginia</h4>
         <br />
@@ -58,10 +80,45 @@ const Testimonials = () => {
           alt="testimonial"
         />
         <p className="testimonial-text">
-          "Our experience with Nicki at TravelBoss in planning our trip to Ireland was amazing. She gave gentle prompts to encourage us to book our stays in a timely manner, and arranged for us to stay in 10 different places across the country. When we had questions or concerns before and during the trip, she was able to troubleshoot and help us while abroad. We will definitely be calling TravelBoss again!"
+          "Our experience with Nicki at TravelBoss in planning our trip to
+          Ireland was amazing. She gave gentle prompts to encourage us to book
+          our stays in a timely manner, and arranged for us to stay in 10
+          different places across the country. When we had questions or concerns
+          before and during the trip, she was able to troubleshoot and help us
+          while abroad. We will definitely be calling TravelBoss again!"
         </p>
         <h4 className="testimonial-author">~ Janice G., Indiana</h4>
-        <br/>
+        <br />
+      </div>
+      <div className="testimonial">
+        <img
+          className="testimonial-image"
+          src="/Images/testimonial5.png"
+          alt="testimonial"
+        />
+        <p className="testimonial-text">
+          "Planning any trip, personal or professional, not only consumes time
+          but also requires keen attention to detail to make the experience
+          memorable. I collaborated with Nicki at TravelBoss to organize a trip
+          to California wine country for a large group of women from a social
+          club in Ohio. While all the ladies shared an interest in visiting
+          Sonoma, their preferences for activities varied. This posed a
+          challenge in coordinating a trip that needed to feel personal and
+          special for each individual.
+          <br />
+          Nicki rose to the challenge! She secured an estate to accommodate our
+          large party, identified vendors to support her proposed schedule, and
+          even successfully gathered all seventeen of us in San Francisco in
+          time for a lunch before heading into the countryside. <br />
+          We appreciated the personalized itineraries that catered to everyone,
+          ranging from group dinners, hiking, and yoga to smaller group tours of
+          the many wineries by chartered vehicles or bicycle! From the moment
+          the group arrived at the estate to the time we left for the airport,
+          Nicki/[company name] went above and beyond to ensure a seamless and
+          memorable experience for all!"
+        </p>
+        <h4 className="testimonial-author">~ Stacey C., Ohio</h4>
+        <br />
       </div>
     </div>
   );
